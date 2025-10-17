3e10eddcde7845c0be455e71eea8dabab4a13b84
toginski <tymekoginski@gmail.com>
2025-10-17 10:18:38 +0200

    .

--- Sklep Internetowy/src/Book.java
+++ Sklep Internetowy/src/Book.java
@@ -5,27 +5,30 @@ import java.util.Scanner;
 public class Book extends Product {
     String genre;
     String author;
-    String powiesc;
+    String type;
     private static List<Book> bookList = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
-    public Book(String name, double price, int id, String category, String genre, String author, String powiesc){
+
+    public Book(String name, double price, int id, String category, String genre, String author, String type){
         super(name, price, id, category);
         this.genre = genre;
         this.author = author;
-        this.powiesc = powiesc;
+        this.type = type;
+
 
     }
 
     public static void initializeBooks() {
         if (bookList.isEmpty()) { // żeby nie duplikować przy ponownym wejściu
-            bookList.add(new Book("Wiedźmin: Ostatnie życzenie", 49.99, 1, "Ksiązki", "fantastyka", "Andrzej Sapkowski", "powiesc"));
-            bookList.add(new Book("Harry Potter i Kamień Filozoficzny", 39.99, 2, "Książki", "fantasy", "J.K. Rowling", "powiesc"));
-            bookList.add(new Book("Zbrodnia i kara", 44.50, 3, "Ksiązki", "klasyka", "Fiodor Dostojewski", "powiesc"));
-            bookList.add(new Book("Mały Książę", 29.90, 4, "Ksiązki", "literatura dziecięca", "Antoine de Saint-Exupéry", "bajka"));
-            bookList.add(new Book("1984", 35.00, 5, "Ksiązki", "dystopia", "George Orwell", "powiesc"));
-            bookList.add(new Book("Hobbit", 42.99, 6, "Ksiązki", "fantasy", "J.R.R. Tolkien", "powiesc"));
-            bookList.add(new Book("Pan Tadeusz", 33.00, 7, "Ksiązki", "epopeja", "Adam Mickiewicz", "klasyka"));
-            bookList.add(new Book("Rok 1984", 38.99, 8, "Ksiązki", "dystopia", "George Orwell", "powiesc"));
+            bookList.add(new Book("Wiedźmin: Ostatnie życzenie", 49.99, 1, "Ksiązki", "fantastyka", "Andrzej Sapkowski", "Powiesc"));
+            bookList.add(new Book("Harry Potter i Kamień Filozoficzny", 39.99, 2, "Książki", "fantasy", "J.K. Rowling", "Powiesc"));
+            bookList.add(new Book("Zbrodnia i kara", 44.50, 3, "Ksiązki", "klasyka", "Fiodor Dostojewski", "Powiesc"));
+            bookList.add(new Book("Mały Książę", 29.90, 4, "Ksiązki", "literatura dziecięca", "Antoine de Saint-Exupéry", "Bajka"));
+            bookList.add(new Book("1984", 35.00, 5, "Ksiązki", "dystopia", "George Orwell", "Powiesc"));
+            bookList.add(new Book("Hobbit", 42.99, 6, "Ksiązki", "fantasy", "J.R.R. Tolkien", "Powiesc"));
+            bookList.add(new Book("Pan Tadeusz", 33.00, 7, "Ksiązki", "epopeja", "Adam Mickiewicz", "Klasyka"));
+            bookList.add(new Book("Rok 1984", 38.99, 8, "Ksiązki", "dystopia", "George Orwell", "Powiesc"));
         }
     }
 
@@ -33,7 +36,7 @@ public class Book extends Product {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Wybierz rodzaj książek ");
-        System.out.println("1. Powieści");
+        System.out.println("1. Powiesci");
         System.out.println("2. Klasyka");
         System.out.println("3. Bajki");
         System.out.println("x. Wróć do menu głównego");
@@ -41,13 +44,13 @@ public class Book extends Product {
         String option = sc.next();
         switch (option) {
             case "1":
-                // powieści
+                showAvailableBookByType("Powiesc");
                 break;
             case "2":
-                // klasyka
+                showAvailableBookByType("Klasyka");
                 break;
             case "3":
-                // bajka
+                showAvailableBookByType("Bajki");
                 break;
             case "x":
                 Main.displayMenu();
@@ -57,6 +60,21 @@ public class Book extends Product {
                 break;
         }
     }
+    public static void showAvailableBookByType(String type) {
+        List<Book> sortedBookList = new ArrayList<>();
+        int counter = 1;
+        for (int i = 0; i < bookList.toArray().length; i++) {
+            Book book = bookList.get(i);
+            if (book.type.equals(type)) {
+                System.out.println(counter+ ". " + book);
+                sortedBookList.add(book);
+                counter++;
+            }
+        }
+        System.out.println("Dodaj produkty do koszyka (wpisz numer produktu)");
+        int index = sc.nextInt();
+        Cart.addToCart(sortedBookList.get(index - 1));
 
+    }
 
 }
